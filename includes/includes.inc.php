fbcd95b2dade84070df15ab839ff5d82fb715415
Nyma Dolatkhahnejad <mustachedeliva@gmail.com>
2017-05-12 11:03:41 +0200

    Changed includes, better check?

--- includes/includes.inc.php
+++ includes/includes.inc.php
@@ -1,3 +1,4 @@
 <?php
-include ('config.inc.php'); //MySQL & PDO Config
-include ('header.inc.php'); //Header & Menu
+include ('config.inc.php'); //PHP & PDO Config
+include ('header.inc.php'); //Header only
+//Menu has to be included further
