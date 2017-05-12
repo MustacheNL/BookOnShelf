5d150942748319691a975e5f4de417b1e4ef1c56
Nyma Dolatkhahnejad <mustachedeliva@gmail.com>
2017-05-12 11:32:29 +0200

    Menu now includes once on every page, so you don't have to change every page's menu with hand. But once in menu.inc.php. Also some better checks added for menu is-active.

---
+++ mybooks.php
@@ -0,0 +1,37 @@
+<?php
+require_once("classes/session.class.php");
+require_once('classes/user.class.php');
+$auth_user = new USER();
+$user_id = $_SESSION['user_session'];
+$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+$stmt->execute(array(":user_id"=>$user_id));
+
+$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+$pagename = "Mijn boeken";
+include "includes/header.inc.php";
+
+if (isset($_POST['login'])) {
+    echo 'login';
+} else if (isset($_POST['register'])) {
+    echo 'register';
+} else {
+    //No button pressed
+}
+include 'includes/menu.inc.php';
+?>
+    <main class="mdl-layout__content" style="margin: auto;">
+        <span class="mdl-chip mdl-chip--contact">
+            <span class="mdl-chip__contact mdl-color--red mdl-color-text--white">!</span>
+            <span class="mdl-chip__text" style="text-align: center">Let op: We hebben nog niet alle informatie over je gekregen! Om het systeem te kunnen gebruiken verzoek wij je <a href='settings.php'>hier</a> alles in te vullen.</span>
+        </span>
+        <main class="mdl-layout__content mdl-color--grey-100" style="display: block;">
+            <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+                Here must come some shit.
+            </div>
+        </main>
+    </main>
+    <?php include 'includes/footer.inc.php' ?>
+    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+</body>
+</html>
